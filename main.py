2bc520e0e6cc0623ad763635f03c6c7fb74d6858
R_Anmol <anmolmain3061@gmail.com>
2026-06-21 22:05:10 +0530

    wip: add feature extraction of three sonic features

--- main.py
+++ main.py
@@ -8,9 +8,28 @@ def audio_extraction(filepath):
     y, sr = librosa.load(filepath)
 
     chroma = librosa.feature.chroma_stft(y=y, sr=sr)
-    chroma_mean = np.mean(chroma, axis=0)
-    chroma_std = np.std(chroma, axis=0)
-    print(chroma)
+    chroma_mean = np.mean(chroma, axis=1)  # 12
+    chroma_std = np.std(chroma, axis=1)  # 12
+
+    rms = librosa.feature.rms(y=y)
+    rms_mean = np.mean(rms)  # 1
+    rms_std = np.std(rms)  # 1
+
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=20)
+    mfcc_mean = np.mean(mfccs, axis=1)
+    mfcc_std = np.std(mfccs, axis=1)
+
+    final_vector = np.concatenate(
+        (
+            chroma_mean,
+            chroma_std,  # 12 + 12 floats
+            [rms_mean, rms_std],  # 1 + 1 float
+            mfcc_mean,
+            mfcc_std,  # 20 + 20 float
+        )
+    )
+
+    return final_vector
 
 
 folderpath = Path("/mnt/Personal_Data/Video/demo_folder/")
@@ -19,4 +38,7 @@ folderpath = Path("/mnt/Personal_Data/Video/demo_folder/")
 
 for filepath in folderpath.iterdir():
     # print(f"Song Name: {filepath.name}")
-    audio_extraction(filepath)
+    h = audio_extraction(filepath)
+    print(h)
+    print(np.shape(h))
+    break
