da43a3172eecd459dc640763e2c50a374fe44a4f
R_Anmol <anmolmain3061@gmail.com>
2026-06-21 13:55:24 +0530

    wip: add chroma sonic feature extraction

--- main.py
+++ main.py
@@ -1,6 +1,22 @@
 from pathlib import Path
 
+import librosa
+import numpy as np
+
+
+def audio_extraction(filepath):
+    y, sr = librosa.load(filepath)
+
+    chroma = librosa.feature.chroma_stft(y=y, sr=sr)
+    chroma_mean = np.mean(chroma, axis=0)
+    chroma_std = np.std(chroma, axis=0)
+    print(chroma)
+
+
 folderpath = Path("/mnt/Personal_Data/Video/demo_folder/")
 # Assuming that only required audio files are inside the given folder
+
+
 for filepath in folderpath.iterdir():
-    print(f"Song Name: {filepath.name}")
+    # print(f"Song Name: {filepath.name}")
+    audio_extraction(filepath)
